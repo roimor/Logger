e6fede953e136c06cf8bba257f88922c7eae3dde
roimor <43300666+roimor@users.noreply.github.com>
2019-01-22 18:26:14 +0200

    Add files via upload

---
+++ log_func.py
@@ -0,0 +1,22 @@
+import logging
+import time
+import datetime
+
+
+class Logger:
+    def __init__(self):
+        self.ts = time.time()
+        self.st = datetime.datetime.fromtimestamp(self.ts).strftime('%Y-%m-%d %H:%M:%S')
+        logging.basicConfig(filename='log_file.log', level=logging.INFO)
+
+    def start(self, text, st):
+        logging.info(st + "==>" + text)
+
+    def success(self, text, st):
+        logging.info(st + "==>" + text)
+
+    def fail(self, text, st):
+        logging.warning(st + "==>" + text)
+
+    def run(self,text):
+        self.start(text,self.st)
